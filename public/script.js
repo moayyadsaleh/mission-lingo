10f4801c9445b9380182e29679059bce763b6821
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-08-14 12:45:41 -0600

    Fix zoom in zoom out functionality, add styling

--- public/script.js
+++ public/script.js
@@ -18,6 +18,39 @@ document.addEventListener("DOMContentLoaded", function() {
       element.style.fontSize = currentFontSize + "px";
     });
   });
+});
 
+const draggable = document.getElementById("draggable");
 
+let isDragging = false;
+let initialX, initialY;
+let offsetX, offsetY;
+
+draggable.addEventListener("touchstart", (e) => {
+  isDragging = true;
+  const touch = e.touches[0];
+  initialX = touch.clientX;
+  initialY = touch.clientY;
+  offsetX = initialX - draggable.getBoundingClientRect().left;
+  offsetY = initialY - draggable.getBoundingClientRect().top;
+  draggable.style.cursor = "grabbing";
+});
+
+document.addEventListener("touchmove", (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  const touch = e.touches[0];
+
+  // Calculate new position
+  const newX = touch.clientX - offsetX;
+  const newY = touch.clientY - offsetY;
+
+  // Update element's position
+  draggable.style.left = newX + "px";
+  draggable.style.top = newY + "px";
+});
+
+document.addEventListener("touchend", () => {
+  isDragging = false;
+  draggable.style.cursor = "grab";
 });
